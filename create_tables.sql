6dc07722107e3aa2e231668f2abc80c2c3b4c11e
Tony11818 <40446163+Tony11818@users.noreply.github.com>
2019-04-30 19:29:08 -0700

    Create create_tables.sql
    Create Tables

---
+++ create_tables.sql
@@ -0,0 +1,104 @@
+-- ---
+-- Table 'admin'
+-- 
+-- ---
+		
+CREATE TABLE `admin` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(50) NULL DEFAULT NULL,
+  `lastname` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'participant'
+-- 
+-- ---
+		
+CREATE TABLE `participant` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(50) NULL DEFAULT NULL,
+  `lastname` VARCHAR(50) NULL DEFAULT NULL,
+  `ismember` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'lake'
+-- 
+-- ---
+		
+CREATE TABLE `lake` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `lakename` VARCHAR(100) NULL DEFAULT NULL,
+  `state` VARCHAR(50) NULL DEFAULT NULL,
+  `city` VARCHAR(75) NULL DEFAULT NULL,
+  `tournament_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'tournament'
+-- 
+-- ---
+		
+CREATE TABLE `tournament` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `date` DATE NULL DEFAULT NULL,
+  `time` TIME NULL DEFAULT NULL,
+  `participant_id` INTEGER NULL DEFAULT NULL,
+  `admin_id` INTEGER NULL DEFAULT NULL,
+  `lake_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'fish'
+-- 
+-- ---
+		
+CREATE TABLE `fish` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `weight` DECIMAL(4,2) NULL DEFAULT NULL,
+  `participant_id` INTEGER NULL DEFAULT NULL,
+  `tournament_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'particpant_tournament_fish'
+-- 
+-- ---
+		
+CREATE TABLE `particpant_tournament_fish` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `participant_id` INTEGER NULL DEFAULT NULL,
+  `tournament_id` INTEGER NULL DEFAULT NULL,
+  `fish_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Table 'payment'
+-- 
+-- ---
+		
+CREATE TABLE `payment` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `paid_for_tournament` INTEGER NULL DEFAULT NULL,
+  `participant_id` INTEGER NULL DEFAULT NULL,
+  `tournament_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) engine=InnoDB Default charset utf8mb4 collate=utf8mb4_unicode_ci;
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `lake` ADD FOREIGN KEY (tournament_id) REFERENCES `tournament` (`id`);
+ALTER TABLE `tournament` ADD FOREIGN KEY (admin_id) REFERENCES `admin` (`id`);
+ALTER TABLE `particpant_tournament_fish` ADD FOREIGN KEY (participant_id) REFERENCES `participant` (`id`);
+ALTER TABLE `particpant_tournament_fish` ADD FOREIGN KEY (tournament_id) REFERENCES `tournament` (`id`);
+ALTER TABLE `particpant_tournament_fish` ADD FOREIGN KEY (fish_id) REFERENCES `fish` (`id`);
+ALTER TABLE `payment` ADD FOREIGN KEY (participant_id) REFERENCES `participant` (`id`);
+ALTER TABLE `payment` ADD FOREIGN KEY (tournament_id) REFERENCES `tournament` (`id`);

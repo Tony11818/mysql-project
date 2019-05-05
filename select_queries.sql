18cac352763004fb87ca93e052405d431c710a4e
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-05-05 12:52:27 -0700

    new join

--- select_queries.sql
+++ select_queries.sql
@@ -6,7 +6,7 @@ JOIN lake AS l ON (l.id=t.lake_id)
 JOIN results AS r ON(r.tournament_id=t.id)\G
 
 SELECT a.firstname + ' ' + a.lastname AS 'admin', l.lakename, l.state, p.firstname, p.lastname, f.weight AS 'Fish weight' FROM people AS a
-JOIN people_tournament_fish AS ptf ON (p.id=ptf.admin_id)
+JOIN people_tournament_fish AS ptf ON (a.id=ptf.admin_id)
 JOIN tournament AS t ON (t.id=ptf.tournament_id)
 JOIN lake AS l ON (l.id=t.lake_id)
 JOIN people AS p ON (p.id=ptf.participant_id)

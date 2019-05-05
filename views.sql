956253ac9b0589ec8c1b873b5bb1409cdf3106f6
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-05-05 16:29:59 -0700

    new join

--- views.sql
+++ views.sql
@@ -1,7 +1,7 @@
 CREATE Algorithm = participantfish AS
 SELECT p.firstname, p.lastname,l.lakename, t.tournament_date, f.weight
 FROM people AS p
-JOIN people_tournament_fish AS ptf (p.id=ptf.participant_id)
+JOIN people_tournament_fish AS ptf ON(p.id=ptf.participant_id)
 JOIN tournament AS t ON (t.id=ptf.tournament_id)
 JOIN fish AS f ON (f.id=ptf.fish_id)
 JOIN lake AS l ON (l.id=t.lake_id)\G

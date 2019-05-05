374572fa440834005d56ec12e26556bb7a23d9b7
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-05-05 14:42:15 -0700

    new join

--- select_queries.sql
+++ select_queries.sql
@@ -12,3 +12,10 @@ JOIN lake AS l ON (l.id=t.lake_id)
 JOIN people AS p ON (p.id=ptf.participant_id)
 JOIN fish AS f ON (f.id=ptf.fish_id)
 WHERE t.id = 1\G
+
+SELECT CONCAT(p.firstname,' ',p.lastname) AS 'fishermen', l.lakename. l.state, f.weight AS 'fish weight' FROM people AS p
+JOIN people_tournament_fish AS ptf ON (p.id=ptf.participant_id)
+JOIN tournament AS t ON (t.id=ptf.tournament_id)
+JOIN fish AS f ON (f.id=ptf.fish_id)
+JOIN lake AS l ON (l.id=t.lake_id)
+WHERE t.id = 1\G

7df222fac767747b2ef88ca56b5638dd710b7a96
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-05-05 15:28:14 -0700

    new join

--- select_queries.sql
+++ select_queries.sql
@@ -5,14 +5,6 @@ JOIN fish AS f ON (f.id=ptf.fish_id)
 JOIN lake AS l ON (l.id=t.lake_id)
 JOIN results AS r ON(r.tournament_id=t.id)\G
 
-SELECT CONCAT(a.firstname,' ',a.lastname) AS 'admin', l.lakename, l.state, p.firstname, p.lastname, f.weight AS 'Fish weight' FROM people AS a
-JOIN tournament AS t ON (a.id=t.admin_id)
-JOIN people_tournament_fish AS ptf ON (t.id=ptf.tournament_id)
-JOIN lake AS l ON (l.id=t.lake_id)
-JOIN people AS p ON (p.id=ptf.participant_id)
-JOIN fish AS f ON (f.id=ptf.fish_id)
-WHERE t.id = 1\G
-
 SELECT CONCAT(p.firstname,' ',p.lastname) AS "fishermen", l.lakename, l.state, f.weight AS "fish weight" FROM people AS p
 JOIN people_tournament_fish AS ptf ON (p.id=ptf.participant_id)
 JOIN tournament AS t ON (t.id=ptf.tournament_id)
@@ -22,5 +14,4 @@ WHERE t.id = 1\G
 
 SELECT CONCAT(a.firstname,' ',a.lastname) AS 'admin', l.lakename, l.state, t.tournament_date FROM people AS a
 JOIN tournament AS t ON (a.id=t.admin_id)
-JOIN lake AS l ON (l.id=t.lake_id)
-WHERE t.admin_id = 1 \G
+JOIN lake AS l ON (l.id=t.lake_id)\G
